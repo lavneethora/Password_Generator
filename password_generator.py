15b01cd668360c667c2d3646bdcead77b8588585
Lavneet Hora <31lavneet@gmail.com>
2024-10-13 14:24:01 -0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,4 @@
 import random
-from lists import letters
-from lists import numbers
-from lists import symbols
 
 password_generator_logo = """
   _____                                    _    _____                           _             
@@ -11,6 +8,10 @@ password_generator_logo = """
  | |  | (_| \__ \__ \\ V  V / (_) | | | (_| | | |__| |  __/ | | |  __/ | | (_| | || (_) | |   
  |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|  \_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|                                                                                        
 """
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print(password_generator_logo)    
 print("Welcome to the PyPassword Generator!")
